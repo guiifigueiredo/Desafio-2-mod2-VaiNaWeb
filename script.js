88a86a123ef373c6d7731ece5418333e27aa3ed6
Guilherme Figueiredo <guilherme_figueiredo@outlook.com.br>
2022-06-29 15:41:39 -0400

    first commit

--- script.js
+++ script.js
@@ -1,42 +1,22 @@
-let idade = 17
-let condicaoHumana = true
-let aninversario = 'setembro'
-let nome = 'ricardo'
-let sobrenome = 'valente'
-//condição 1
-if (idade > 18){
-    console.log("você é maior de idade");
-}else{
-    console.log("você é menor de idade");
+//loop 1
+for(let i =1; i <= 10; i++){
+    console.log(i);
 }
 
+//loop 2
+let i = 11
 
-//condição 2
-if (idade >= 18 && condicaoHumana == true){
-    console.log("você é maior de idade e está em boas condições");
-}else{
-    console.log("você não é maior de idade e não está em boas condições");
+while(i > 0){
+    i--
+    console.log(i);
 }
 
-
-//condição 3
-if(aninversario == 'janeiro' || aninversario == 'dezembro'){
-    console.log("o seu aniversário é em janeiro ou dezembro");
-}else{
-    console.log("o seu aniversário não é em janeiro ou dezembro");
+//loop 3
+for(let i = 1; i <= 100; i+=2){
+    console.log(i);
 }
 
-
-//condição 4
-if(nome[0] == 'r'){
-    console.log("o seu nome começa com a letra R")
-}else(
-    console.log("o seu nome não começa com a letra R")
-)
-
-//condição 5
-if(sobrenome.length > 6){
-    console.log("o seu sobrenome tem mais de 6 letras");
-}else{
-    console.log("o seu sobrenome não tem 6 letras");
+//loop 4
+for(let i = 0; i <= 100; i+=2){
+    console.log(i);
 }
